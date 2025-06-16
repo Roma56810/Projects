7abf287c677e2e4cbcfefb136b6d42dfb4889111
Roma56810 <roma56810.sites@gmail.com>
2025-06-16 17:34:32 +0300

    *

--- game/server.js
+++ game/server.js
@@ -4,29 +4,34 @@ import {v4 as uuid} from "uuid";
 import {writeFile, readFileSync, existsSync} from "fs";
 const clients = {};
 const log = existsSync('log') && readFileSync('log', 'utf-8');
-const message = log ? JSON.parse(log) : [];
+const messages = log ? JSON.parse(log) : [];
+
 const wss = new Server({port: 8000});
-wss.on("connection",(ws)=>{
+wss.on("connection", (ws) => {
     const id = uuid();
     clients[id] = ws;
-    console.log(`New client &{id}`);
+
+    console.log(`New client ${id}`);
     ws.send(`Ваш UUID: ${id}`);
     ws.send(JSON.stringify(messages));
-    ws.on('message',(rawMessage)=>{
+
+    ws.on('message', (rawMessage) => {
         const {name, message} = JSON.parse(rawMessage);
         messages.push({name, message});
         for (const id in clients) {
             clients[id].send(JSON.stringify([{name, message}]))
         }
     })
-    ws.on('close',() => {
+
+    ws.on('close', () => {
         delete clients[id];
         console.log(`Client is closed ${id}`)
     })
 })
-process.on('SIGINT',() => {
+
+process.on('SIGINT', () => {
     wss.close();
-    writeFile('log',JSON.stringify(messages), err => {
+    writeFile('log', JSON.stringify(messages), err => {
         if (err) {
             console.log(err);
         }
